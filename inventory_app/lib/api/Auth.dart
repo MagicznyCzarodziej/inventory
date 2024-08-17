25b616eb08cee6824915cb6443377db21beceb91
Przemysław Pitus <nexit07@gmail.com>
2024-08-17 20:26:35 +0200

    Remove Flutter app

--- inventory_app/lib/api/Auth.dart
+++
@@ -1,9 +0,0 @@
-import 'package:inventory_app/api/HttpClient.dart';
-
-Future login() async {
-   await HttpClient().client.post('/auth/login', data: {'username': 'deko', 'password': 'deko'});
-}
-
-Future refreshToken() async {
-   await HttpClient().client.post('/auth/refresh-token');
-}
--- inventory_app/lib/routes/simpleRoute.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-PageRoute<T> simpleRoute<T>(Widget route) {
-  return PageRouteBuilder<T>(
-    pageBuilder: (_, __, ___) => route,
-    transitionDuration: Duration.zero,
-    reverseTransitionDuration: Duration.zero,
-  );
-}
